fa12fbe825dd51f29bc2fc581ee93da56baa6211
Karol-Caicedo <160459214+Karol-Caicedo@users.noreply.github.com>
2024-04-16 23:48:47 -0500

    Update ParqueaderoCM.cpp

--- ParqueaderoCM.cpp
+++ ParqueaderoCM.cpp
@@ -23,71 +23,70 @@ int registrar(){
     char Opc_vehiculo[20];
     int volver_registrar = 0;
     
-    do{
-        cout<<"Bienvenido al parqueadero el callejon negro"<<endl;
-        cout<<"\n";
+ do{
+    cout<<"Bienvenido al parqueadero el callejon negro"<<endl;
+    cout<<"\n";
         
-        //pide memoria
-        aux = (struct vehiculo *) malloc (sizeof(struct vehiculo));
+    //pide memoria
+    aux = (struct vehiculo *) malloc (sizeof(struct vehiculo));
+    cout<<"Ingresa el nombre del propietario del vehiculo: "<<endl;
+    cin>>aux->nombre;    
+    cout<<"\n";
 
-        cout<<"Ingresa el nombre del propietario del vehiculo: "<<endl;
-        cin>>aux->nombre;    
-        cout<<"\n";
+    cout<<"Ingresa la placa del vehiculo: "<<endl;
+    cin>>aux->placa;
 
-        cout<<"Ingresa la placa del vehiculo: "<<endl;
-        cin>>aux->placa;
-
-        do{
-        	cout<<"Escribe el tipo de vehiculo tal cual (Coche / Moto): "<<endl;
-        	cin>>aux->tipo_vehiculo;
-			volver = 0;
-			if (aux->tipo_vehiculo[0] == coche[0]){
-				if(aux->tipo_vehiculo[1] == coche[1]){
-					if(aux->tipo_vehiculo[2] == coche[2]){
-						Caja_parqueadero = Caja_parqueadero + 1000;
-					}else{
-						cout << "Opcion Invalida, Escriba la opcion tal cual (Coche / Moto)..." << endl;
-		        		volver = 1;
-					}
-				}else{
-					cout << "Opcion Invalida, Escriba la opcion tal cual (Coche / Moto)..." << endl;
-		        	volver = 1;
-				}
-		    }else if (aux->tipo_vehiculo[0] == moto[0]){
-					if(aux->tipo_vehiculo[1] == moto[1]){
-						if(aux->tipo_vehiculo[2] == moto[2]){
-							Caja_parqueadero = Caja_parqueadero + 500;
-						}else{
-							cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
-		        			volver = 1;
-						}
-					}else{
-						cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
-		        		volver = 1;
-					}
-		    }else {
-		        cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
-		        volver = 1; 
-		    }
-		}while(volver == 1);
-        
-        //Almacenar en Lifo
-        if(cab==NULL){
-            cab = aux;
-            cab->sig = NULL;
-        }else{
-            aux->sig =cab;
-            cab = aux;
+     do{
+        cout<<"Escribe el tipo de vehiculo tal cual (Coche / Moto): "<<endl;
+        cin>>aux->tipo_vehiculo;
+	volver = 0;
+	if (aux->tipo_vehiculo[0] == coche[0]){
+	    if(aux->tipo_vehiculo[1] == coche[1]){
+		if(aux->tipo_vehiculo[2] == coche[2]){
+  	            Caja_parqueadero = Caja_parqueadero + 1000;
+		}else{
+		     cout << "Opcion Invalida, Escriba la opcion tal cual (Coche / Moto)..." << endl;
+		     volver = 1;
+		}
+	    }else{
+		 cout << "Opcion Invalida, Escriba la opcion tal cual (Coche / Moto)..." << endl;
+		 volver = 1;
+		 }
+	}else if (aux->tipo_vehiculo[0] == moto[0]){
+		if(aux->tipo_vehiculo[1] == moto[1]){
+		   if(aux->tipo_vehiculo[2] == moto[2]){
+		      Caja_parqueadero = Caja_parqueadero + 500;
+		   }else{
+		      cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
+		      volver = 1;
+	           }
+	       }else{
+		      cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
+		      volver = 1;
+		}
+	}else {
+		cout << "Escriba la opcion tal cual (Coche / Moto)..." << endl;
+	        volver = 1; 
         }
-        aux = NULL;
-        free(aux);
+}while(volver == 1);
+        
+ //Almacenar en Lifo
+ if(cab==NULL){
+    cab = aux;
+    cab->sig = NULL;
+}else{
+    aux->sig =cab;
+    cab = aux;
+}
+ aux = NULL;
+ free(aux);
 
-        cout<<"El registro ha sido exitoso"<<endl;
-        system("pause");
-        cout<<"Desea hacer otro registro? (1.SI/2.NO)"<<endl;
-        cin>>volver_registrar;
-    }while(volver_registrar!=2);
-    return 0;
+ cout<<"El registro ha sido exitoso"<<endl;
+ system("pause");
+ cout<<"Desea hacer otro registro? (1.SI/2.NO)"<<endl;
+ cin>>volver_registrar;
+}while(volver_registrar!=2);
+return 0;
 }
 
 
@@ -95,16 +94,16 @@ int mostrar(){
     if(cab!=NULL){
     	aux = cab;
     	while(aux!=NULL){
-    		cout<<"-> Propietario: "<<aux->nombre<<endl;
+    	    cout<<"-> Propietario: "<<aux->nombre<<endl;
             cout<<"Placa: "<<aux->placa<<endl;
             cout<<"Tipo de vehiculo: "<<aux->tipo_vehiculo<<endl;
             cout<<"------------------------------------------------------"<<endl;
-    		aux = aux->sig;
-		}
+    	    aux = aux->sig;
+	}
 		
         cout<<"Dinero en Caja: $"<<Caja_parqueadero<<endl;
         system("pause"); 
-    }else{
+  }else{
         cout<<"Vacio..."<<endl;
         cout<<"No hay Vehiculos en el parqueadero"<<endl;
         cout<<"Dinero en Caja: $"<<Caja_parqueadero<<endl;
@@ -115,10 +114,10 @@ int mostrar(){
 
 int retirar(){
 	//Eliminar tipo Lifo
-	if(cab!=NULL){
-		//Datos
-		aux = cab;
-		cout<<"-> Propietario: "<<aux->nombre<<endl;
+    if(cab!=NULL){
+	//Datos
+	aux = cab;
+	cout<<"-> Propietario: "<<aux->nombre<<endl;
         cout<<"Placa: "<<aux->placa<<endl;
         cout<<"Tipo de vehiculo: "<<aux->tipo_vehiculo<<endl;
         cout<<"Este vehiculo se eliminara..."<<endl;
@@ -126,16 +125,16 @@ int retirar(){
         
         //Si es un Coche se suma 100 a la caja
         if (aux->tipo_vehiculo[0] == coche[0]){
-			Caja_parqueadero = Caja_parqueadero + 100;
-		}else{ //Si es una moto se le suma 50 a la caja
-			Caja_parqueadero = Caja_parqueadero + 50;
-		}
+	   Caja_parqueadero = Caja_parqueadero + 100;
+	}else{ //Si es una moto se le suma 50 a la caja
+	     Caja_parqueadero = Caja_parqueadero + 50;
+	}
 		
-		//Elimina vehículo
-		cab = cab->sig;
-		free(aux);
+	//Elimina vehículo
+	cab = cab->sig;
+	free(aux);
 		
-		cout<<"Vehiculo eliminado..."<<endl;
+	cout<<"Vehiculo eliminado..."<<endl;
         system("pause");	
 	}
 	return 0;
